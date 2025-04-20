f70bba1c4b28c2fe360d1ea6b6d6810f02c783fa
zi1o1a <zilolanader@gmail.com>
2025-04-20 16:33:14 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
   return (
     <>
-      <div className='h-screen text-text bg-background1'>
+      <div className='h-screen text-text bg-background1 relative z-40'>
         <div className='relative z-10 h-10 bg-primary'>
           Hello
           <div className=''>
@@ -53,6 +53,33 @@ const App = () => {
           <p className='text-center'>Skills and Projects</p>
           <img src='/icons/down-arrow.svg' className='h-10 m-auto'></img>
         </div>
+      </div>
+      <div className='h-screen'>
+        <div>
+          <div className='absolute z-10 ml-32 mt-16 h-[85%] w-[36%] bg-primary'>
+          Projects
+        </div>
+        <div className='absolute z-10 mt-16 ml-[50%]'>
+          <div className='text-3xl mt-32 ml-28'>Technical Skills</div>
+          <div className='flex mt-10 ml-28'>
+            <div>
+              <p className='my-1'>JavaScript</p>
+              <p className='my-1'>HTML</p>
+              <p className='my-1'>CSS</p>
+              <p className='my-1'>React.js</p>
+            </div>
+            <div className='ml-16'>
+              <p className='my-1'>Python</p>
+              <p className='my-1'>Node.js</p>
+              <p className='my-1'>Next.js</p>
+              <p className='my-1'>Tailwind CSS</p>
+            </div>
+          </div>
+          <div className='text-3xl mt-32 ml-28'>Languages</div>
+          <div className='ml-28 mt-8'>English C1 (IELTS Academic)</div>
+        </div>
+        </div>
+        <div className='absolute right-bg2 z-0 -rotate-45 bg-background2'></div>
       </div>
     </>
   )
