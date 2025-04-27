43ea2172d0e721f7e045ede08b9f9363b533b2ac
zi1o1a <zilolanader@gmail.com>
2025-04-27 18:15:39 +0500

    updated

--- src/components/SkillsAndProjects.jsx
+++ src/components/SkillsAndProjects.jsx
@@ -9,6 +9,27 @@ const SkillsAndProjects = ( {isDarkMode} ) => {
           <div className='absolute z-20 ml-32 mt-16 h-[85%] w-[36%] bg-primary'>
             <img src='/icons/full-screen.svg' className='opacity-10 hover:h-8 hover:m-2 hover:opacity-40 float-right h-6 m-3'></img>
             <p className='text-3xl m-6'>Projects</p>
+            <div className='flex ml-6'>
+              <div className='bg-teal-500 h-40 w-40 rounded-3xl text-center pt-14'>img placeholder</div>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>Typing Speed Test</p>
+                <p className='mt-1'>info</p>
+              </div>
+            </div>
+            <div className='flex ml-6 my-10'>
+              <div className='bg-teal-500 h-40 w-40 rounded-3xl text-center pt-14'>img placeholder</div>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>E-Shop</p>
+                <p className='mt-1'>info</p>
+              </div>
+            </div>
+            <div className='flex ml-6'>
+              <div className='bg-teal-500 h-40 w-40 rounded-3xl text-center pt-14'>img placeholder</div>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>Snake Game</p>
+                <p className='mt-1'>info</p>
+              </div>
+            </div>
           </div>
           <div className='absolute z-20 mt-16 ml-[50%]'>
             <div className='text-3xl mt-32 ml-28'>Technical Skills</div>
