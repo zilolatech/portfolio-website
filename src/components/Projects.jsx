3c56c5e0debaaf9227d3f590252ab3564aefc390
zi1o1a <zilolanader@gmail.com>
2025-04-28 19:04:01 +0500

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='absolute z-20 ml-32 mt-16 h-[85%] w-[36%] bg-primary'>
+            <img src='/icons/full-screen.svg' className='opacity-10 hover:h-8 hover:m-2 hover:opacity-40 float-right h-6 m-3'></img>
+            <p className='text-3xl m-6'>Projects</p>
+            <div className='flex ml-6 pt-3'>
+              <img src='./images/typing-speed-test.png' className='h-40 rounded'></img>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>Typing Speed Test</p>
+                <p className='mt-1'>info</p>
+              </div>
+            </div>
+            <div className='flex ml-6 my-8'>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>E-Shop</p>
+                <p className='mt-1'>info</p>
+              </div>
+              <img src='./images/e-shop.png' className='h-40 ml-16 rounded'></img>
+            </div>
+            <div className='flex ml-6'>
+              <img src='./images/snake-game.jpg' className='h-40 rounded-2xl'></img>
+              <div className='ml-5 mt-2'>
+                <p className='text-xl'>Snake Game</p>
+                <p className='mt-1'>info</p>
+              </div>
+            </div>
+          </div>
   )
 }
