0256d46939def5aa9ea3122825f08d91a6af88a3
zi1o1a <zilolanader@gmail.com>
2025-04-29 17:50:00 +0500

    fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,21 +6,21 @@ export const Projects = () => {
             <img src='/icons/full-screen.svg' className='opacity-10 hover:h-8 hover:m-2 hover:opacity-40 float-right h-6 m-3'></img>
             <p className='text-3xl m-6'>Projects</p>
             <div className='flex ml-6 pt-3'>
-              <img src='./images/typing-speed-test.png' className='h-40 rounded'></img>
+              <img src='./images/typing-speed-test.png' className='h-40 w-44 object-cover rounded-2xl'></img>
               <div className='ml-5 mt-2'>
                 <p className='text-xl'>Typing Speed Test</p>
                 <p className='mt-1'>info</p>
               </div>
             </div>
             <div className='flex ml-6 my-8'>
+              <img src='./images/e-shop.png' className='h-40 w-44 object-cover rounded-2xl'></img>
               <div className='ml-5 mt-2'>
                 <p className='text-xl'>E-Shop</p>
                 <p className='mt-1'>info</p>
               </div>
-              <img src='./images/e-shop.png' className='h-40 ml-16 rounded'></img>
             </div>
             <div className='flex ml-6'>
-              <img src='./images/snake-game.jpg' className='h-40 rounded-2xl'></img>
+              <img src='./images/snake-game.jpg' className='h-40 w-44 object-cover rounded-2xl'></img>
               <div className='ml-5 mt-2'>
                 <p className='text-xl'>Snake Game</p>
                 <p className='mt-1'>info</p>
