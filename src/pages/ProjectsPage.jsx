e24181cc1468be72da4a3bf8189de93ba937a3fd
zi1o1a <zilolanader@gmail.com>
2025-05-15 18:17:18 +0500

    updated

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ProjectsPage = () => {
   return (
-    <div>ProjectsPage</div>
+    <>
+      <div className='ml-10 mt-5 text-3xl'>Projects</div>
+      <div className='flex mt-10 justify-center'>
+        <div className='mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-slate-500'></div>
+        <div className='mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-slate-500'></div>
+        <div className='mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-slate-500'></div>
+      </div>
+    </>
   )
 }
 
