e0d5b9407307e193839b91e29af54953d36e6494
zi1o1a <zilolanader@gmail.com>
2025-05-22 18:41:51 +0500

    updated

--- src/components/SkillsAndProjects.jsx
+++ src/components/SkillsAndProjects.jsx
@@ -11,7 +11,7 @@ const SkillsAndProjects = ( {isDarkMode, backgroundPosition} ) => {
           <Projects />
           <div className='absolute z-20 mt-16 ml-[50%]'>
             <div className='text-3xl mt-32 ml-28'>Technical Skills</div>
-            <div className='flex mt-10 ml-28'>
+            <div className='flex mt-6 ml-28'>
               <div>
                 <p className='my-1 flex'><img src='/icons/javascript.svg' width='20px' className='mr-2'></img>JavaScript</p>
                 <p className='my-2 flex'><img src='/icons/html5.svg' width='20px' className='mr-2'></img>HTML</p>
@@ -25,8 +25,11 @@ const SkillsAndProjects = ( {isDarkMode, backgroundPosition} ) => {
                 <p className='my-2 flex'><img src='/icons/tailwind-css.svg' width='20px' className='mr-2'></img>Tailwind CSS</p>
               </div>
             </div>
-            <div className='text-3xl mt-28 ml-28'>Languages</div>
-            <div className='ml-28 mt-8'>English C1 (IELTS Academic)</div>
+            <div className='text-3xl mt-14 ml-28'>Certifications</div>
+            <div className='ml-28 mt-6'><a href='#' target='_blank'>Responsive Web Design, freeCodeCamp, 2024</a></div>
+            <div className='ml-28 mt-2'><a href='#' target='_blank'>JavaScript Algorithms and Data Structure, freeCodeCamp, 2024</a></div>
+            <div className='text-3xl mt-16 ml-28'>Languages</div>
+            <div className='ml-28 mt-6'>English C1 (IELTS Academic), Russian, Uzbek</div>
           </div>
         </div>
         <div className={`absolute right-bg2 z-10 -rotate-45 ml-[-150px] ${!isDarkMode ? 'bg-background2' : 'hidden'}`} style={{transition: 'all 0.3s ease', marginTop: backgroundPosition}}></div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const ProjectsPage = ({isDarkMode}) => {
   return (
     <>
-      <div className='text-text bg-background1 h-[100vh]'>
-        <div className='ml-10 pt-5 text-3xl z-10'>Projects</div>
-        <div className='flex mt-10 justify-center z-10'>
-          <div className='z-10 mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
-            <img src='./images/typing-speed-test.png' className='w-[80%] m-5 rounded-xl'></img>
+      <div className='text-text bg-background1 md:h-[100vh] mb-10 md:mb-0'>
+        <div className='md:ml-10 ml-5 md:pt-5 pt-3 text-3xl z-10'>Projects</div>
+        <div className='md:flex mt-10 justify-center z-10'>
+          <div className='rounded-md md:rounded-none z-20 mt-10 h-[60vh] md:w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
+            <img src='./images/typing-speed-test.png' className='w-[80%] md:m-5 m-3 pt-2 md:pt-0 rounded-md md:rounded-xl'></img>
             <div className='inline-block float-right text-2xl -rotate-90 mr-[-80px] opacity-50'>Typing Speed Test</div>
             <div className='ml-7 w-[70%]'>
               <p className='font-medium text-xl'>Key Features</p>
@@ -17,11 +17,11 @@ const ProjectsPage = ({isDarkMode}) => {
                 <li>- words-per-minute (WPM) calculations</li>
               </ul>
               <p className='mt-4'>React.js, Tailwind.CSS, JavaScript</p>
-              <p className='absolute bottom-52 font-medium flex'>GitHub: <a href='https://github.com/zilolatech/typing-speed-test' target='_blank' className='font-thin ml-2'>typing-speed-test</a></p>
+              <p className='md:absolute bottom-52 mt-10 font-medium flex'>GitHub: <a href='https://github.com/zilolatech/typing-speed-test' target='_blank' className='font-thin ml-2'>typing-speed-test</a></p>
             </div>
           </div>
-          <div className='z-10 mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
-            <img src='./images/e-shop.png' className='w-[80%] m-5 rounded-xl'></img>
+          <div className='rounded-md md:rounded-none z-10 mt-10 h-[60vh] md:w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
+            <img src='./images/e-shop.png' className='w-[80%] md:m-5 m-3 pt-2 md:pt-0 rounded-md md:rounded-xl'></img>
             <div className='inline-block float-right text-2xl -rotate-90 mr-[-66px] opacity-50'>E-Shop Website</div>
             <div className='ml-5 w-[85%]'>
               <p className='font-medium text-xl'>Key Features</p>
@@ -32,14 +32,14 @@ const ProjectsPage = ({isDarkMode}) => {
                 <li>- mobile friendly</li>
               </ul>
               <p className='mt-4'>React.js, Tailwind.CSS, JavaScript, Resposive Design</p>
-              <p className='absolute bottom-52 font-medium flex'>GitHub: <a href='https://github.com/zilolatech/eshop-tailwind' target='_blank' className='font-thin ml-2'>eShop</a></p>
+              <p className='md:absolute bottom-52 mt-10 font-medium flex'>GitHub: <a href='https://github.com/zilolatech/eshop-tailwind' target='_blank' className='font-thin ml-2'>eShop</a></p>
             </div>
           </div>
-          <div className='z-10 mt-10 h-[60vh] w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
-            <img src='./images/snake-game.jpg' className='w-[80%] m-5 rounded-xl'></img>
-            <div className='inline-block float-right text-2xl -rotate-90 mr-[-170px] opacity-50'>Snake Game with Color Elements</div>
-            <div className='ml-5 w-[70%] fixed'>
-              <p className='font-medium text-xl overflow-hidden text-nowrap w-[40%]'>Key Features</p>
+          <div className='rounded-md md:rounded-none z-10 mt-10 md:h-[60vh] h-[70vh] md:w-[25%] mx-10 hover:h-[70vh] hover:w-[30%] hover:mt-4 hover:mx-1 bg-primary'>
+            <img src='./images/snake-game.jpg' className='w-[80%] md:m-5 m-3 pt-2 md:pt-0 rounded-md md:rounded-xl'></img>
+            <div className='inline-block float-right text-2xl -rotate-90 mr-[-50px] opacity-50'>Snake Game</div>
+            <div className='ml-5 w-[80%]'>
+              <p className='font-medium text-xl'>Key Features</p>
               <ul className='mt-2'>
                 <li>- dynamic color-changing elements</li>
                 <li>- smooth gameplay mechanics</li>
@@ -47,11 +47,11 @@ const ProjectsPage = ({isDarkMode}) => {
                 <li>- touch support (mobile friendly)</li>
               </ul>
               <p className='mt-4'>React.js, Tailwind.CSS, JavaScript</p>
-              <p className='font-medium mt-[72px] flex'>GitHub: <a href='https://github.com/zilolatech/snakegame' target='_blank' className='font-thin ml-2'>Snake Game</a></p>
+              <p className='md:absolute bottom-52 mt-10 font-medium flex'>GitHub: <a href='https://github.com/zilolatech/snakegame' target='_blank' className='font-thin ml-2'>Snake Game</a></p>
             </div>
           </div>
         </div>
-        <div className={`fixed right-bg2 z-0 -rotate-45 ${!isDarkMode ? 'bg-background2' : 'hidden'}`} style={{top: '500px', right: '-700px'}}></div>
+        <div className={`hidden md:inline fixed right-bg2 z-0 -rotate-45 ${!isDarkMode ? 'bg-background2' : 'hidden'}`} style={{top: '500px', right: '-700px'}}></div>
       </div>
     </>
   )
